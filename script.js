b30c3ecd45e9a119fd41983e7350f84f4729a96c
Muhibbulloh <225215017+muhib137@users.noreply.github.com>
2025-08-17 21:07:51 +0500

    Make it functional :)

--- script.js
+++ script.js
@@ -21,6 +21,40 @@ const OPERATIONS = {
   "/": divide,
 };
 
-function operate(op, num1, num2){
+function operate(op, num1, num2) {
+  console.log(`num1: ${num1}, num2: ${num2}`);
   return OPERATIONS[op](num1, num2);
 }
+
+// SELECT UI ELEMENTS
+const numberBtns = document.querySelectorAll(".btn--num");
+const display = document.querySelector(".display p");
+const operationBtns = document.querySelectorAll(".btn--op");
+const equalBtn = document.querySelector(".btn--equal");
+
+let num1, num2;
+
+Array.from(numberBtns).forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    display.innerText += e.target.innerText;
+    if (num2 === null) {
+      num2 = parseInt(display.innerText);
+    } else {
+      num1 = parseInt(display.innerText);
+    }
+  });
+});
+
+let operand;
+Array.from(operationBtns).forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    display.innerText = "";
+    operand = e.target.innerText;
+    num2 = null;
+  });
+});
+
+equalBtn.addEventListener("click", () => {
+  num1 = display.innerText = operate(operand, num1, num2);
+});
+
